c7c667cc1ccf4e997a0c1862e0cc40600b25f0b7
DavidDyberg <david.dyberg@edu.cmeducations.se>
2024-01-10 12:45:53 +0100

    create global styles and styles registry

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
+import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import StyledComponentsRegistry from '@/lib/registry'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +17,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <StyledComponentsRegistry>
+        <body className={inter.className}>
+          {children}
+        </body>
+      </StyledComponentsRegistry>
     </html>
   )
 }
