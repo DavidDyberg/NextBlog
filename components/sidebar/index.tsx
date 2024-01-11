455fe2b617d93283a5b3fb4b00471f02bc58f25b
DavidDyberg <david.dyberg@edu.cmeducations.se>
2024-01-11 13:37:54 +0100

    removed empty row

--- components/sidebar/index.tsx
+++ components/sidebar/index.tsx
@@ -4,19 +4,19 @@ import Link from 'next/link'
 const SideBar = () => {
     return (
         <Styles.SidebarContainer>
-      <Link href="/">
-        <Styles.SidebarItem $primary>Blog</Styles.SidebarItem>
-      </Link>
-      <Link href="/about">
-        <Styles.SidebarItem $primary>About</Styles.SidebarItem>
-      </Link>
-      <Link href="/create-post">
-        <Styles.SidebarItem $primary>Create Post</Styles.SidebarItem>
-      </Link>
-      <Link href="/login">
-        <Styles.SidebarItem $primary>Login</Styles.SidebarItem>
-      </Link>
-    </Styles.SidebarContainer>
+            <Link href="/">
+              <Styles.SidebarItem $primary>Blog</Styles.SidebarItem>
+            </Link>
+            <Link href="/about">
+              <Styles.SidebarItem $primary>About</Styles.SidebarItem>
+            </Link>
+            <Link href="/create-post">
+              <Styles.SidebarItem $primary>Create Post</Styles.SidebarItem>
+            </Link>
+            <Link href="/login">
+              <Styles.SidebarItem $primary>Login</Styles.SidebarItem>
+            </Link>
+        </Styles.SidebarContainer>
     )
 }
 
